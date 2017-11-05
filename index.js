08dd23a1edf4cb77e49f2e2d52911ce931a42d31
Andree Ray <andreer@kgh.local>
2017-11-05 20:43:08 +0100

    initial release
    Committer: develdoe <andreeray@live.com>

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 module.exports = {
-    
+    post: function() {
+        var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments))
+        var options = args.pop()
+        var httpRequest
+
+        document.getElementById(options.id).addEventlistener('click', makeRequest)
+
+        var makeRequest = function(options) {
+            httpRequest = new XMLHttpRequest()
+
+            if(!httpRequest) {
+                console.log('Giving up :( Cannot create an XMLHTTP instance')
+                return false
+            }
+
+            httpRequest.onreadystatechange = logContents
+            httpRequest.open(options.request, options.url, options.async)
+            httpRequest.send()
+        }
+
+        var logContents = function() {
+            if(httpRequest.readyState === XMLHttpRequest.DONE) {
+                if(httpRequest.status === 200) {
+                    console.log(httpRequest.responseText)
+                } else {
+                    console.log('There was a problem with the request.')
+                }
+            }
+        }
+    }
 }
